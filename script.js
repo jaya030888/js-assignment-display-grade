02f3f817f1eca97d1723a576578fc384505b9a58
Jaya Patel <jayamyname19@gmail.com>
2025-10-29 09:56:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ let age=prompt("Enter Your Age:");
 let marks=prompt("Enter Your Marks:");
 let totalMarks=prompt("Enter Total Marks:");
 
-if(age<=0 & age>=120){
+if(age<=0 || age>=120){
     age= "Invalid Age"
 }
 
 let percentage = (marks/totalMarks)*100;
 let grade=""
 
-if(percentage<=100 & percentage>=90){
+if(percentage<=100 && percentage>=90){
     grade="Grade A"
     let message="Congratulations you got "+ grade + ". Keep Working Hard"
     
 }
 
-else if(percentage<90 & percentage>=80){
+else if(percentage<90 && percentage>=80){
     grade="Grade B"
     message= "You got "+ grade + ". Work Hard. You have more Potential."
 }
 
-else if(percentage<80 & percentage>=60){
+else if(percentage<80 && percentage>=60){
     grade="Grade C"
     message= "You got "+ grade + ". Need Work Hard. You have the Potential."
 }
 
-else if(percentage<60 & percentage>=30){
+else if(percentage<60 && percentage>=30){
     grade="Grade D"
     message= "You got "+ grade + ". Needs Improvement. Have to Work Very Hard."
 }
 
 
-else if(percentage<30 & percentage>=0){
+else if(percentage<30 && percentage>=0){
     grade="Failed"
     message= "You have "+ grade + ". Very poor performance. Needs to Work Very Hard."
 }
@@ -45,3 +45,4 @@ else{
 
 let studentData=(`Name: ${name}<br>Age: ${age}<br>Grade: ${grade}<br>${message}`);
 document.getElementById("result").innerHTML = studentData 
+
